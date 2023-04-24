6dc9bf38de733f9d9bc9d3d53d180d5fab698241
penpentan <66662244+penpentan@users.noreply.github.com>
2023-04-24 08:51:35 +0000

    [Revise] favo func

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to:"homes#top"
-  resources :users, only: [:edit,:show,:update]
+  get "/homes/about" => "homes#about" ,as: "about"
   resources :post_images,only: [:new,:create,:index,:show,:destroy] do
-    resource :favorites, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
     resources :post_comments,only: [:create,:destroy]
   end
-  get "/homes/about" => "homes#about" ,as: "about"
+  resources :users, only: [:edit,:show,:update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
